1b325a92d5e2f0f18377a2b3e818a1acc3315a26
injune1123 <injune1123@Juns-Air.home>
2015-12-23 11:04:57 -0500

    added two solustions

--- Day.3-3findPrimeFactors.js
+++ Day.3-3findPrimeFactors.js
@@ -1,3 +1,5 @@
+// write a function to find all the prime factors of a number
+// e.g
 //8[2] 
 //6[2,3]
 //9[3]
@@ -6,35 +8,54 @@
 // n, n
 
 
-function isPrime(Arr,n){
-    for(var j=0; j<Arr.length; j++){
-        if (n%Arr[j]===0){
-            return false
-        }
+
+// O(n)
+// function primeFactors(n){
+//     var divisor=2;
+//     var result=[];
+//     while(n>2){
+//         if(n%divisor===0){
+//             if(result.indexOf(divisor)==-1){
+//             result.push(divisor)
+//             }
+//             n=n/divisor
+//         }
+//         else{
+//             divisor++
+//         }
+//     }
+//     return result
+// }
+
+
+
+//O(n^2)
+// function isNotPrime(Arr,n){
+//     for(var j=0; j<Arr.length; j++){
+//         if (n%Arr[j]===0){
+//             return true
+//         }
         
-    }
-    return true
-}
+//     }
+//     return false
+// }
 
 
 
-function findPrimeFactors (n){
+// function findPrimeFactors (n){
 	
-	var primeFactorArr = [];
-	for (var i=2; i<=n; i++){
-		if (n%i===0&&isPrime(primeFactorArr,i)){
-			primeFactorArr.push(i);
-		}
-	}
-
-if (primeFactorArr.length>1){
-    primeFactorArr.pop();
-    
-}
-return primeFactorArr;
-
-}
+// 	var primeFactorArr = [];
+// 	for (var i=2; i<=n; i++){
+// 		if (n%i===0&&!isNotPrime(primeFactorArr,i)){
+// 			primeFactorArr.push(i);
+// 		}
+// 	}
 
 
-findPrimeFactors(9)
+// return primeFactorArr;
+
+// }
+
+
+// findPrimeFactors(9)
 
