99791f3ce13d958043e61580bca6c7eab2ffb1aa
injune1123 <injune1123@gmail.com>
2020-07-18 01:07:52 -0400

    Add solution

---
+++ validate-binary-search-tree.js
@@ -0,0 +1,48 @@
+// Validate Binary Search Tree
+//https://leetcode.com/problems/validate-binary-search-tree/
+// Given a binary tree, determine if it is a valid binary search tree (BST).
+
+// Assume a BST is defined as follows:
+
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+ 
+
+// Example 1:
+
+//     2
+//    / \
+//   1   3
+
+// Input: [2,1,3]
+// Output: true
+// Example 2:
+
+//     5
+//    / \
+//   1   4
+//      / \
+//     3   6
+
+// Input: [5,1,4,null,null,3,6]
+// Output: false
+// Explanation: The root node's value is 5 but its right child's value is 4.
+
+var isValidBST = function(root) {
+    return helper(root, null, null)
+};
+
+function helper (node, rightLower, leftUpper) {
+
+    if(node == null) return true
+    
+    const val = node.val
+    if (rightLower !== null && val <= rightLower) return false;
+    if (leftUpper !== null && val >= leftUpper) return false;
+
+    if (! helper(node.right, val, leftUpper)) return false;
+    if (! helper(node.left, rightLower, val)) return false;
+    return true;
+
+}
